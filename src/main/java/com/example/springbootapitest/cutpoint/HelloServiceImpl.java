78bac808cadb2df7e25b1cbd14437b8f15554f80
zhangdx <zdongxiao@outlook.com>
2023-09-12 15:21:35 +0800

    测试aop

--- src/main/java/com/example/springbootapitest/cutpoint/HelloServiceImpl.java
+++ src/main/java/com/example/springbootapitest/cutpoint/HelloServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.springbootapitest.cutpoint;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
